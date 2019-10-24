d328d926c00dae9b11d205f35b653bdd455efb60
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-24 19:13:52 +0200

    register user

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,12 +8,15 @@ const posts = require('./routes/api/posts');
 
 const db = require('./config/keys').mongoURI;
 
+const app = express();
+
 mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log('MongoDB Connected'))
 	.catch((err) => console.log(err));
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
