7345fc6851f84fa0c5ff8952f31927642844923b
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-24 17:47:22 +0200

    create routes: test routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const db = require('./config/keys').mongoURI;
 
 mongoose
@@ -13,6 +17,11 @@ const app = express();
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
+
+// use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
