077d6b7a5a4ec36f8547fc9779263b401a9cc182
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-10-25 10:13:37 +0200

    login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,7 +14,7 @@ router.get('/test', (req, res) => {
 	});
 });
 
-// @route   GET api/users/register
+// @route   POST api/users/register
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
@@ -40,6 +40,40 @@ router.post('/register', (req, res) => {
 			});
 		}
 	});
+});
+
+// @route   POST api/users/login
+// @desc    Login user / Returning the JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// Find user by email
+	User.findOne({ email })
+		.then((user) => {
+			// Check for user
+			if (!user) {
+				return res.status(404).json({
+					email: 'User not found'
+				});
+			}
+			console.log('User -> ', user);
+			// Check password
+			bcrypt
+				.compare(password, user.password)
+				.then((isMatch) => {
+					if (isMatch) {
+						return res.json({ msg: 'Success' });
+					} else {
+						return res.status(400).json({
+							password: 'Password Incorrect'
+						});
+					}
+				})
+				.catch();
+		})
+		.catch();
 });
 
 module.exports = router;
